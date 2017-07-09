58ed5c5e39a9aed8532e84751f903c8ff1594b0a
NickitaX <nickita.shulgin@yahoo.com>
2017-07-09 22:14:08 +1000

    Test API and Add User

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,44 @@ var express = require('express');
 var router = express.Router();
 var url_module = require('url');
 
-var table_name = 'geotags';
+var geotags_table_name = 'geotags';
+var users_table_name = 'users';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yula Server App' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Yula Server App'});
 });
 
 /* GET Locations API */
-router.get('/api/locations', function(req, res, next) {
+router.get('/api/locations', function (req, res, next) {
+
     var db = req.database;
-    var collection = db.collection(table_name);
-    collection.find({},{},function(err,result){
+    var collection = db.collection(geotags_table_name);
+
+    collection.find({}, {}, function (err, result) {
         if (err) throw err;
+
         var name = result;
+
         db.close();
+
         res.send(name);
     });
 });
 
-router.get('/api/addLocation', function(req, res, next) {
+router.get('/api/test/user_count', function (req, res, next) {
+    var db = req.database;
+    var collection = db.collection(users_table_name);
+    collection.find({}, {}, function (err, result) {
+        if (err) throw err;
+
+        db.close();
+
+        res.send(result.length+"");
+    });
+});
+
+router.get('/api/add_location', function (req, res, next) {
 
     var parsed_url = url_module.parse(req.url, true);
 
@@ -31,12 +50,38 @@ router.get('/api/addLocation', function(req, res, next) {
     var p_lng = parsed_url.query.lng;
     var p_userid = parsed_url.query.userid;
 
-    var location = {lat:p_lat, lng:p_lng, userid: p_userid };
+    var location = {lat: p_lat, lng: p_lng, userid: p_userid};
 
     mongo.connect(db_url, function (err, db) {
         if (err) throw err;
 
-        db.collection(table_name).insertOne(location, function (err, res) {
+        db.collection(geotags_table_name).insertOne(location, function (err, res) {
+            if (err) throw err;
+            db.close();
+        });
+        res.send('Inserted!');
+    });
+});
+
+router.get('/api/add_user', function (req, res, next) {
+
+    var parsed_url = url_module.parse(req.url, true);
+
+    var mongo = req.mongo;
+    var db_url = req.db_url;
+
+    var p_username = parsed_url.query.username;
+    var p_first_name = parsed_url.query.first_name;
+    var p_last_name = parsed_url.query.last_name;
+    var p_user_id = parsed_url.query.user_id;
+    var p_rating = 0;
+
+    var user = {username: p_username, first_name: p_first_name, last_name: p_last_name, user_id: p_user_id, rating: p_rating};
+
+    mongo.connect(db_url, function (err, db) {
+        if (err) throw err;
+
+        db.collection(users_table_name).insertOne(user, function (err, res) {
             if (err) throw err;
             db.close();
         });
